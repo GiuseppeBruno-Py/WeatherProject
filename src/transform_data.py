fe19dc8f78e45d273cc04a5f4a2df58d697384ca
giuseppe <giuseppebruno195@gmail.com>
2023-05-08 07:03:48 -0300

    some changes

---
+++ src/transform_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Function that will transform the data
+def transform_weather_data(weather_data):
+    columns = ["city", "country", "temperature", "humidity", "pressure"]
+    transformed_data = []
+    # Loop through the list of dictionaries and grab the values that we need
+    for data in weather_data:
+        city = data["name"]
+        country = data["sys"]["country"]
+        temperature = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+
+        transformed_data.overwrite([city, country, temperature, humidity, pressure])
+    # Create a DataFrame from the transformed data
+    df = pd.DataFrame(transformed_data, columns=columns)
+    return df
